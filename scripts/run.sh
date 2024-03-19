77397268c032a40015b2e8f64cfff7069e697991
MohammadReza Aliverdiloo <mohammadRezaaliverdiloo@gmail.com>
2024-03-20 02:05:34 +0330

    rebuild add postgresql to setting,modified Dockerfile and Docker compose

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
-#!/bin/bash/
+#!/bin/sh/
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8003
+python manage.py runserver 0.0.0.0:8000
--- src/app/settings/base_settings.py
+++ src/app/settings/base_settings.py
@@ -75,9 +75,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg3',
+        'POSTGRES_DB':'djshop',
+        'PSTGRES_USER':'djad',
+        'POSTGRES_PASSWORD':'p@SSW0RD',
     }
 }
 
