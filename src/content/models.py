f6b1a0a6e6726be055496adc279d7b950ab6a2c7
mohammadreza aliverdiloo <mohammadrezaaliverdiloo@gmail.com>
2024-04-05 04:08:39 +0330

    some change in product and content

--- src/content/models.py
+++ src/content/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from category.models import Category
+class Content(models.Model):
+    
+    class ContentStatus(models.TextChoice):
+        PUBLISHED= 'publishe'
+        DRAFT= 'draft'
+    
+    title= models.CharField(max_length=255,verbose_name=_("title"),help_text=_("title for content"))
+    content= models.TextField(verbose_name=_("content"),help_text=_("write content"))
+    category= models.ForeignKey(
+        Category,related_name="category-content",verbose_name=_("category"),help_text=_("category for content")
+        )
+    author= models.ForeignKey(
+        settings.AUTH_USER_MODEL,related_name="author-content",verbose_name=_("author"),help_text=_("author for content")
+        )
+    status= models.CharField(
+        choices=ContentStatus.choices,default=ContentStatus.PUBLISHED,verbose_name=_("content"),help_text=_("write content")
+                             )
--- src/product/models.py
+++ src/product/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 # import datetime
@@ -16,21 +17,51 @@ class Product(models.Model):
         DELETE = "delete"
         
     
-    name = models.CharField(verbose_name = _("name"),max_length=255,null=False,help_text="not more than 255 character")
-    description = models.TextField(verbose_name = _("description"),null=True,help_text="a bit or more descriprtion about product")
-    brand = models.ForeignKey(Brand,_("brand"),related_name="brand",on_delete = models.CASCADE,help_text="barnd of product")
-    price = models.ForeignKey(Price,related_name="prices",verbose_name=_("price"),on_delete = models.CASCADE)
-    quantity = models.ForeignKey(Quantity,related_name="quantity",verbose_name=_("quantity"),on_delete = models.CASCADE)
-    category = models.ForeignKey(Category,related_name="categories",verbose_name=_("category"),on_delete = models.CASCADE)
-    color=models.ForeignKey(to='Color',related_name="colors",verbose_name=_("brand"),on_delete = models.CASCADE)
-    material = models.CharField(verbose_name = _("material"),max_length=255,null=False)
+    name = models.CharField(
+        verbose_name = _("name"),max_length=255,null=False,help_text="not more than 255 character"
+        )
+    seller = models.ForeignKey(
+        settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="user_relation",verbose_name=_("seller")
+        )
+    description = models.TextField(
+        verbose_name = _("description"),null=True,help_text="a bit or more descriprtion about product"
+        )
+    author= models.ForeignKey(
+        settings.AUTH_USER_MODEL,related_name="author-content",verbose_name=_("author"),help_text=_("author for content")
+        )
+    brand = models.ForeignKey(
+        Brand,_("brand"),related_name="brand",on_delete = models.CASCADE,help_text="barnd of product"
+        )
+    price = models.ForeignKey(
+        Price,related_name="prices",verbose_name=_("price"),on_delete = models.CASCADE
+        )
+    quantity = models.ForeignKey(
+        Quantity,related_name="quantity",verbose_name=_("quantity"),on_delete = models.CASCADE
+        )
+    category = models.ForeignKey(
+        Category,related_name="categories",verbose_name=_("category"),on_delete = models.CASCADE
+        )
+    color=models.ForeignKey(
+        to='Color',related_name="colors",verbose_name=_("brand"),on_delete = models.CASCADE
+        )
+    material = models.CharField(
+        verbose_name = _("material"),max_length=255,null=False
+        )
     weight = models.FloatField()
     dimensions =models.FloatField()
-    tags=models.CharField(max_length=255,verbose_name=_("tags"))
+    tags=models.CharField(
+        max_length=255,verbose_name=_("tags")
+        )
     #image //create an image app 
-    status = models.CharField(_("status"),choices=Status.choices,default=Status.IN_ACTIVE)
-    date_created=models.DateTimeField(auto_now_add = True,verbose_name=_("created_at"))
-    date_modified=models.DateTimeField(auto_now=True,verbose_name=_("modified_at"))
+    status = models.CharField(
+        verbose_name=_("status"),choices=Status.choices,default=Status.IN_ACTIVE
+        )
+    date_created=models.DateTimeField(
+        auto_now_add = True,verbose_name=_("created_at")
+        )
+    date_modified=models.DateTimeField(
+        auto_now=True,verbose_name=_("modified_at")
+        )
 
     class Meta:
         verbose_name="product"
